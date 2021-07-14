7f31e4915cbf06293d6ab54aa6ccfcd5f56fd835
Prachi2617 <36615941+Prachi2617@users.noreply.github.com>
2021-07-14 23:23:11 +0530

    Add files via upload

---
+++ inorder_traversal_recursive.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    // Function to return a list containing the inorder traversal of the tree.
+    void inorder(Node *root, vector<int>&ans)
+    {
+        if(root==NULL)
+        {
+            return ;
+        }
+        inorder(root->left,ans);
+        ans.push_back(root->data);
+        inorder(root->right,ans);
+    }
+    vector<int> inOrder(Node* root)
+    {
+        vector<int>ans;
+        inorder(root,ans);
+        return ans;
+    }
+};
+
