14ffcc63404ccda7ace15be83721b3ff48502d7e
Prachi2617 <36615941+Prachi2617@users.noreply.github.com>
2021-08-02 16:58:18 +0530

    Add files via upload

---
+++ check_for_balanced_trees.cpp
@@ -0,0 +1,22 @@
+//if height of left subtree and right subtree is greater than 1 then it is not balanced
+int func(Node *root, int &ans)
+{
+    if(!root)
+        return 0;
+    int x=func(root->left,ans);
+    int y=func(root->right,ans);
+    if(abs(x-y)>1)
+        {
+            ans=0;
+
+        }
+        return (max(x,y)+1) ;
+}
+
+bool isBalanced(Node *root)
+{
+    int ans=1;
+    int x;
+    x=func(root,ans);
+    return ans;
+}
