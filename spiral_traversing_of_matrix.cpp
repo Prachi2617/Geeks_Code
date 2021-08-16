46c183039af49688da59780f2a08c7957f69328f
Prachi2617 <36615941+Prachi2617@users.noreply.github.com>
2021-08-16 22:24:30 +0530

    Update spiral_traversing_of_matrix.cpp

--- spiral_traversing_of_matrix.cpp
+++ spiral_traversing_of_matrix.cpp
@@ -1,4 +1,5 @@
-	vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c)
+// Time Complexity- O(r*c) space complexity- O(r*c)
+vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c)
     {
         vector<int>ans;
         vector<vector<bool>>vis(r,vector<bool>(c,false));
@@ -30,3 +31,33 @@
         return ans;
 
     }
+
+//Time Complexity- O(r*c) space complexity- 1
+vector<int> spirallyTraverse(vector<vector<int> > matrix, int r, int c)
+{
+	vector<int>ans;
+        int r1=0,c1=0,i;
+        while(r1<r && c1<c)
+        {
+            for(i=c1;i<c;i++)
+                ans.push_back(matrix[r1][i]);
+            r1++;
+            for(i=r1;i<r;i++)
+                ans.push_back(matrix[i][c-1]);
+            c--;
+            if(r1<r)
+            {
+                for(i=c-1;i>=c1;i--)
+                    ans.push_back(matrix[r-1][i]);
+                r--;
+            }
+            if(c1<c)
+            {
+                for(i=r-1;i>=r1;i--)
+                    ans.push_back(matrix[i][c1]);
+                c1++;
+            }
+        }
+        return ans;
+        
+}
